552b809341e18151b4a48670395f5ecff1bd6ab3
Brandon Foley <brandonfoley13@gmail.com>
2026-04-15 18:41:34 -0400

    website change

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://blog.aks.azure.com',
+  url: 'https://bfoley13.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AKS/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'pauldotyu', // Usually your GitHub org/user name.
+  organizationName: 'bfoley13', // Usually your GitHub org/user name.
   projectName: 'aks-blog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
